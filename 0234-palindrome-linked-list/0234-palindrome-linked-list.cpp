a48432352a364049345400c8b05ce29b7449d844
GouravShDev <74348508+GouravShDev@users.noreply.github.com>
2022-10-21 11:52:29 +0530

    Time: 308 ms (74.23%), Space: 110.3 MB (95.70%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,44 +10,23 @@
  */
 class Solution {
 public:
-    pair<ListNode*, ListNode*> getMiddleAndPrev(ListNode* node){
-        if(!node) return {NULL, NULL};
-        ListNode* slow = node;
-        ListNode* fast = node;
-        ListNode* prev= NULL;
-        while(fast && fast->next){
-            prev = slow;
-            slow = slow->next;
-            fast= fast->next->next;
-        }
-        return {prev, slow};
-    }
-    ListNode* reverseLL(ListNode* head){
-        if(!head || !head->next) return head;
-        ListNode* newHead = reverseLL(head->next);
-        head->next->next = head;
-        head->next = NULL;
-        return newHead;
-    }
     bool isPalindrome(ListNode* head) {
-        auto [middlePrev , middle] = getMiddleAndPrev(head);
-        ListNode* rvHead = reverseLL(middle);
-        auto reverseHead = rvHead;
-        ListNode* orgHead = head;
-        while(head && reverseHead){
-            if(head->val != reverseHead->val){
-                return false;
-            }
-            head = head->next;
-            reverseHead=  reverseHead->next;
+        ListNode* slow = head;
+        ListNode* fast= head;
+        ListNode* prev = NULL;
+        while(fast && fast->next){
+            fast = fast->next->next;
+            ListNode* temp = slow->next;
+            slow->next = prev;
+            prev= slow;
+            slow = temp;    
         }
-        ListNode* rvvHead = reverseLL(rvHead);
-        if(middlePrev) middlePrev ->next = rvvHead;
-        // while(orgHead){
-        //     cout<<orgHead->val<<" ";
-        //     orgHead=orgHead->next;
-        // }
+        if(fast) slow= slow->next;
+        while(prev && slow){
+            if(prev->val != slow->val)return false;
+            prev = prev->next;
+            slow = slow->next;
+        }
         return true;
-        
     }
 };
