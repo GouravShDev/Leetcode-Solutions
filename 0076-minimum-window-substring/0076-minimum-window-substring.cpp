e51456ca02a203cd7b7c77ebbf6d181f997658b8
GouravShDev <74348508+GouravShDev@users.noreply.github.com>
2022-10-23 10:10:11 +0530

    Time: 24 ms (72.64%), Space: 8.2 MB (29.37%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int counter = t.size();
-        vector<int> curMp(256);
-        int i =0;
+        vector<int> targetMp(256), curMp(256);
         for(auto &ch : t){
-            curMp[ch]++;
+            targetMp[ch]++;
         }
         int start = 0;
         int end = INT_MAX;
-        
-        // A = 1 , B = 1 , C= 1
-        // a , freq = 0;
-        for(int j =0 ; j < s.size(); j++){
-            if(curMp[s[j]]-- > 0) counter--;
-            while(counter == 0){
+        int counter = 0;
+        int i = 0;
+        for(int j = 0; j < s.size() ; j++){
+            if(targetMp[s[j]]){
+                if(curMp[s[j]] < targetMp[s[j]]) counter++;
+                curMp[s[j]]++;
+            }
+            while(counter == t.size()){
                 if(end - start > j - i){
                     start = i;
                     end = j;
                 }
-                if(curMp[s[i]]++ == 0 )counter++;
+                if(targetMp[s[i]] > 0){
+                    curMp[s[i]]--;
+                    if(curMp[s[i]] < targetMp[s[i]]){
+                        counter--;
+                    }
+                }
                 i++;
             }
-            
         }
-        
-        
-        return end != INT_MAX ? s.substr(start , end - start  +1) : "";
+        return end != INT_MAX? s.substr(start ,end-start + 1) :"";
     }
 };
