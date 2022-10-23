c9e24a3592348af87afe3a07d56c0ed6b60a4c21
GouravShDev <74348508+GouravShDev@users.noreply.github.com>
2022-10-23 10:19:09 +0530

    Time: 20 ms (78.82%), Space: 7.8 MB (68.03%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,33 +1,38 @@
-class Solution {
-public:
-    string minWindow(string s, string t) {
-        vector<int> targetMp(256), curMp(256);
-        for(auto &ch : t){
-            targetMp[ch]++;
-        }
-        int start = 0;
-        int end = INT_MAX;
-        int counter = 0;
-        int i = 0;
-        for(int j = 0; j < s.size() ; j++){
-            if(targetMp[s[j]]){
-                if(curMp[s[j]] < targetMp[s[j]]) counter++;
-                curMp[s[j]]++;
+class Solution
+{
+    public:
+        string minWindow(string s, string t)
+        {
+            vector<int> curMp(256);
+            for (auto &ch: t)
+            {
+                curMp[ch]++;
             }
-            while(counter == t.size()){
-                if(end - start > j - i){
-                    start = i;
-                    end = j;
-                }
-                if(targetMp[s[i]] > 0){
-                    curMp[s[i]]--;
-                    if(curMp[s[i]] < targetMp[s[i]]){
+            int start = 0;
+            int end = INT_MAX;
+            int counter = 0;
+            int i = 0;
+            for (int j = 0; j < s.size(); j++)
+            {
+                if (curMp[s[j]] > 0) counter++;
+                curMp[s[j]]--;
+
+                while (counter == t.size())
+                {
+                    if (end - start > j - i)
+                    {
+                        start = i;
+                        end = j;
+                    }
+
+                    if (curMp[s[i]] == 0)
+                    {
                         counter--;
                     }
+                    curMp[s[i]]++;
+                    i++;
                 }
-                i++;
             }
+            return end != INT_MAX ? s.substr(start, end - start + 1) : "";
         }
-        return end != INT_MAX? s.substr(start ,end-start + 1) :"";
-    }
 };
