3a398ada4c8925f5dbe2f01161d0fe88e5901203
GouravShDev <74348508+GouravShDev@users.noreply.github.com>
2022-10-31 22:09:38 +0530

    Time: 1567 ms (38.67%), Space: 16 MB (17.00%) - LeetHub

---
+++ 0212-word-search-ii/0212-word-search-ii.cpp
@@ -0,0 +1,65 @@
+class Node{
+public:
+    Node* links[26];
+    bool isEnd = false;
+    
+    bool contains(char c){
+        return links[c-'a'] != NULL;
+    }
+    void setLink(char c){
+        links[c-'a'] = new Node();
+    }
+    Node* getLink(char c){
+        return links[c-'a'];
+    }
+    void setEnd(){
+        isEnd = true;
+    }
+    bool getEnd(){
+        return isEnd;
+    }
+    
+};
+class Solution {
+public:
+    Node* root;
+    vector<string> ans;
+    unordered_set<string> st;
+     void solve(vector<vector<char>> &board , int i , int j, Node* node, string cur){
+        if(i < 0 || j < 0 || i >= board.size() || j >= board[0].size() || board[i][j] == '-' || !node->contains(board[i][j])) return;
+         char oldVal = board[i][j];
+        board[i][j] = '-';
+         node = node->getLink(oldVal);
+         cur+= oldVal;
+         if(node->getEnd() && st.find(cur) == st.end()){
+             ans.push_back(cur);
+             st.insert(cur);
+         }
+     solve(board,  i + 1, j, node, cur); solve(board, i-1, j , node, cur);
+            solve(board, i, j+1, node , cur);solve(board , i, j-1, node , cur);
+        board[i][j] = oldVal;
+        
+    }
+    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+        root = new Node();
+        // insert
+        for(auto &word : words){
+            Node* node = root;
+            for(int i =0 ; i < word.size() ; i++){
+                if(!node->contains(word[i])){
+                    node->setLink(word[i]);
+                }
+                node = node->getLink(word[i]);
+            }
+            node->setEnd();
+        }
+        for(int i =0 ; i < board.size() ; i++){
+            for(int j = 0 ; j < board[0].size() ; j++){
+                solve(board, i, j, root, "");
+                if(st.size() == words.size()) return ans;
+            }
+        }
+        
+        return ans;
+    }
+};
