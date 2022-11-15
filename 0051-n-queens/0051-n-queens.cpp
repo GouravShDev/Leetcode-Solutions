3edbf74ccb84b1a1425ada5431842d63adf1ba13
GouravShDev <74348508+GouravShDev@users.noreply.github.com>
2022-11-16 01:03:25 +0530

    Time: 24 ms (30.72%), Space: 20.3 MB (18.76%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,50 +1,37 @@
 class Solution {
 public:
-    bool isPossible(vector<vector<int>> board, int i , int j ){
+    bool isPossible(vector<string> board, int i , int j ){
         int x = i, y = j;
         while(i >= 0 && j >= 0){
-            if(board[i--][j--]){
+            if(board[i--][j--] == 'Q'){
                 return false;
             }
         }
         while(x < board.size() && y < board.size()){
-            if(board[x--][y++]){
+            if(board[x--][y++] == 'Q'){
                 return false;
             }
         }
         return true;
     }
-    void solve(vector<vector<int>> &board, int index, vector<int> &col, vector<vector<string>> &ans){
+    void solve(vector<string> &board, int index, vector<int> &col, vector<vector<string>> &ans){
         if(index == board.size()){
-            vector<string> curVec;
-            for(int i = 0; i < board.size() ; i++){
-                string cur = "";
-                for(int j = 0; j < board.size() ; j++){
-                    if(board[i][j]){
-                        cur += 'Q';
-                    }else{
-                        cur += '.';
-                    }
-                }
-                curVec.push_back(cur);
-                
-            }
-            ans.push_back(curVec);
+            ans.push_back(board);
             return;
         }
         for(int i = 0; i < board.size() ; i++){
             if(!col[i] && isPossible(board, index, i)){
-                board[index][i] = 1;
+                board[index][i] = 'Q';
                 col[i] = 1;
                 solve(board,  index+1, col , ans);
                 col[i] = 0;
-                board[index][i] = 0;
+                board[index][i] = '.';
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
-        vector<vector<int>> board(n , vector<int>(n));
+        vector<string> board(n , string(n,'.'));
         vector<int> colTaken(n);
         solve(board, 0 , colTaken, ans);
         return ans;
