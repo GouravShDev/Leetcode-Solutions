b1a03e3e2d1f972e94ca6baec64f85333b4940ac
GouravShDev <74348508+GouravShDev@users.noreply.github.com>
2022-11-17 22:12:04 +0530

    Time: 21 ms (40.31%), Space: 9.6 MB (37.53%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,29 +1,16 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        vector<int> mp(33);
-        for(auto &num : nums){
-            if(num < 0) mp[32]++;
-            long long x = abs(num);
-            for(int i = 0; i < 32 ; i++){
-                int mask = 1 << i;
-                if((x & mask) > 0){
-                    mp[i]++;
-                }
+        sort(nums.begin(), nums.end());
+        int i = 0;
+        while(i < nums.size()-1){
+            if(nums[i] == nums[i+1] && nums[i+1] == nums[i+2]){
+                i+= 3;
+            }else{
+                return nums[i];
             }
+            
         }
-        // 30th bit
-        long long ans = 0;
-        for(int i =0 ;i < 32; i++){
-            int it =  mp[i];
-            it = it % 3;
-            // cout<<it<<" ";
-            if(it)
-                ans |= (1 << i);
-        }
-        if(mp[32] % 3){
-            return -ans;
-        }
-        return ans;
+        return nums[nums.size()-1];
     }
 };
