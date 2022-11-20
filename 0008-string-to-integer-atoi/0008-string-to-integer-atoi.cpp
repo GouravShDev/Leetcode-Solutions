6f18f2871d703fa373ddb7e0d7d195b533629087
GouravShDev <74348508+GouravShDev@users.noreply.github.com>
2022-11-20 21:21:55 +0530

    Time: 8 ms (42.49%), Space: 7.1 MB (52.83%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,24 +1,38 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int i = 0;
+        
         int ans = 0;
+        int i = 0;
         bool isNeg = false;
-        while(i < s.size() && s[i] == ' ')i++;
-        if(i < s.size() && (s[i] == '-' || s[i] == '+')){
-            isNeg = (s[i++] == '-');
+        
+        // ignore whitespaces
+        
+        while(i < s.size() && s[i] == ' ') i++;
+        
+        // check for sign
+        if(i < s.size() && (s[i] == '+' || s[i] == '-')){
+            if(s[i++] == '-'){
+                isNeg = true;
+            }
         }
+        
         while(i < s.size() && s[i] >= '0' && s[i] <= '9'){
-            if(INT_MAX/10 < ans || (INT_MAX / 10 == ans && s[i] > '7') ){
+            // check for overflow
+            // 24.......
+            // 214748364    7
+            if( INT_MAX / 10 < ans || ( INT_MAX /10 == ans && s[i] > '7')){
                 if(isNeg){
                     return INT_MIN;
                 }else{
                     return INT_MAX;
                 }
             }
+            
             ans *= 10;
             ans += s[i++] - '0';
         }
+        
         return isNeg ? -ans : ans;
     }
 };
