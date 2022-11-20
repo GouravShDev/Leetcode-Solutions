7633bbb6222157bb16d9d409806a2fef3daee3ab
GouravShDev <74348508+GouravShDev@users.noreply.github.com>
2022-11-20 20:39:57 +0530

    Time: 9 ms (32.05%), Space: 7.1 MB (52.83%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
     int myAtoi(string s) {
-         int ans = 0;
-        int i =0 ;
+        int i = 0;
+        int ans = 0;
         bool isNeg = false;
-        while(i < s.size()){
-            
-            // ignore space
-            while(i < s.size() && s[i] == ' ') i++;
-            if( i < s.size() && (s[i] == '-' || s[i] == '+')){
-                isNeg = (s[i++] == '-');
-            }
-            while(i < s.size() && s[i] >= '0' && s[i] <= '9'){
-                if(!isNeg &&(( (INT_MAX  )/ 10 < ans) || ((s[i] - '0' > 7 && (INT_MAX  )/ 10 == ans)))){
-                    return INT_MAX;
-                }else if( isNeg && ( (INT_MIN )/ 10 > -ans|| ( INT_MIN/ 10 == -ans && s[i] - '0' > 7))){
-                    //cout << ((INT_MIN  ) / 10 + (s[i] - '0'))<<endl;
+        while(i < s.size() && s[i] == ' ')i++;
+        if(i < s.size() && (s[i] == '-' || s[i] == '+')){
+            isNeg = (s[i++] == '-');
+        }
+        while(i < s.size() && s[i] >= '0' && s[i] <= '9'){
+            if(INT_MAX/10 < ans || (INT_MAX / 10 == ans && s[i] > '7') ){
+                if(isNeg){
                     return INT_MIN;
+                }else{
+                    return INT_MAX;
                 }
-                ans *= 10;
-                ans += s[i++] - '0';
             }
-            if(i < s.size() && (s[i] <= '0' || s[i] >= '9')){
-                break;
-            }
-            
+            ans *= 10;
+            ans += s[i++] - '0';
         }
         return isNeg ? -ans : ans;
     }
