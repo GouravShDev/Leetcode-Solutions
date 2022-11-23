952cf2acd41b3db75f8b4c9f76c2157336061f04
GouravShDev <74348508+GouravShDev@users.noreply.github.com>
2022-11-23 13:43:28 +0530

    Time: 20 ms (44.68%), Space: 9.6 MB (61.51%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -3,25 +3,16 @@ public:
     int singleNumber(vector<int>& nums) {
         int ans = 0;
         for(int i = 0; i < 32; i++){
-            
-            int count = 0;
-            for( int num  : nums){
-                if((num & (1 << i))){
-                    count++;
+            int x = 0;
+            for(auto &num : nums){
+                if(num & (1 << i)){
+                    x++;
                 }
             }
-            count %= 3;
-            if(count > 0){
-                ans |= 1 << i;
+            if(x % 3){
+                ans |= (1 << i);
             }
         }
-        //  for(int i = 0; i < 32; i++){
-        //     if(ans & (1 << i) > 0)
-        //         cout<<"1";
-        //     else{
-        //         cout<<"0";
-        //     }
-        // }
         return ans;
     }
 };
