1244303eb1c46daba87a40b0b1d7865cb7f32296
GouravShDev <74348508+GouravShDev@users.noreply.github.com>
2022-11-25 16:52:32 +0530

    Time: 107 ms (92.55%), Space: 43.1 MB (21.84%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
-  int sumSubarrayMins(vector<int>& A) {
-    stack<pair<int, int>> in_stk_p, in_stk_n;
-    // left is for the distance to previous less element
-    // right is for the distance to next less element
-    vector<int> left(A.size()), right(A.size());
-		
-    //initialize
-    for(int i = 0; i < A.size(); i++) left[i] =  i + 1;
-    for(int i = 0; i < A.size(); i++) right[i] = A.size() - i;
-		
-    for(int i = 0; i < A.size(); i++){
-      // for previous less
-      while(!in_stk_p.empty() && in_stk_p.top().first > A[i]) in_stk_p.pop();
-      left[i] = in_stk_p.empty()? i + 1: i - in_stk_p.top().second;
-      in_stk_p.push({A[i],i});
-			
-      // for next less
-      while(!in_stk_n.empty() && in_stk_n.top().first > A[i]){
-        auto x = in_stk_n.top();in_stk_n.pop();
-        right[x.second] = i - x.second;
-      }
-      in_stk_n.push({A[i], i});
+    int mod = 1e9+7;
+    int sumSubarrayMins(vector<int>& arr) {
+        int n = arr.size();
+        
+        vector<int> prev(n,-1);
+        vector<int> next(n,n);
+        stack<int> stk;
+        for(int i =0 ;i < n; i++){
+            while(!stk.empty() && arr[stk.top()] >= arr[i]){
+                next[stk.top()] = i;
+                stk.pop();
+            }
+            stk.push(i);
+        }
+        for(int i =n-1;i >= 0; i--){
+            while(!stk.empty() && arr[stk.top()] > arr[i]){
+                prev[stk.top()] = i;
+                stk.pop();
+            }
+            stk.push(i);
+        }
+        int ans = 0;
+        for(int i =0;i < n ; i++){
+            
+            int len = ((next[i]-i) * (i - prev[i] * 1LL) % mod);
+            // cout<<len<<" " ;
+            ans +=  (arr[i] * 1LL * len)  % mod;
+            ans %= mod;
+        }
+        return ans;
     }
-
-    int ans = 0, mod = 1e9 +7;
-    for(int i = 0; i < A.size(); i++){
-      ans = (ans + 1ll * A[i]*left[i]*right[i])%mod;
-    }
-    return ans;
-  }
 };
