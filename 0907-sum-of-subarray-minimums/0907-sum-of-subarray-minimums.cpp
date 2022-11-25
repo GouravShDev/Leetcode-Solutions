73b148e2439da135e6192fcdb1812fdb6f6f8073
GouravShDev <74348508+GouravShDev@users.noreply.github.com>
2022-11-25 16:44:02 +0530

    Time: 271 ms (22.93%), Space: 44.1 MB (13.34%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -1,36 +1,44 @@
+
 #define ll long long
-class Solution {
-public:
-    int mod = 1e9+7;
-    int sumSubarrayMins(vector<int>& arr) {
-        int n  = arr.size();
-        vector<ll> prev(n,-1);
-        vector<ll> next(n,n);
-        stack<ll> stk;
-        for(ll i =0 ;i < n; i++){
-            while(!stk.empty() && arr[stk.top()] >= arr[i]){
-                next[stk.top()] = i;
-                stk.pop();
+class Solution
+{
+    public:
+        int mod = 1e9 + 7;
+    int sumSubarrayMins(vector<int> &A)
+    {
+        stack<pair<int, int>> in_stk_p, in_stk_n;
+       	// left is for the distance to previous less element
+       	// right is for the distance to next less element
+        vector<int> left(A.size()), right(A.size());
+
+       	//initialize
+        for (int i = 0; i < A.size(); i++) left[i] = i + 1;
+        for (int i = 0; i < A.size(); i++) right[i] = A.size() - i;
+
+        for (int i = 0; i < A.size(); i++)
+        {
+           	// for previous less
+            while (!in_stk_p.empty() && in_stk_p.top().first > A[i]) in_stk_p.pop();
+            left[i] = in_stk_p.empty() ? i + 1 : i - in_stk_p.top().second;
+            in_stk_p.push({ A[i],
+                i });
+
+           	// for next less
+            while (!in_stk_n.empty() && in_stk_n.top().first > A[i])
+            {
+                auto x = in_stk_n.top();
+                in_stk_n.pop();
+                right[x.second] = i - x.second;
             }
-            stk.push(i);
+            in_stk_n.push({ A[i],
+                i });
         }
-        for(ll i =n-1;i >= 0; i--){
-            while(!stk.empty() && arr[stk.top()] > arr[i]){
-                prev[stk.top()] = i;
-                stk.pop();
-            }
-            stk.push(i);
-        }
-        ll ans = 0;
-        for(ll i =0;i < n ; i++){
-            
-            ll len = ((next[i]-i) % mod * (i - prev[i] ) % mod) % mod;
-            // if(len < 0)
-            ans += (arr[i]* len) % mod;
-            // if(ans < 0){
-                // cout<<ans;
-            // }
-        }
-        return ans % mod;
+
+        int ans = 0, mod = 1e9 + 7;
+        for (int i = 0; i < A.size(); i++)
+        {
+            ans = (ans+ ( 1ll *  A[i] * left[i] *right[i] )%mod)%mod;
+        } 
+        return ans;
     }
 };
