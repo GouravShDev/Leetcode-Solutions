f77c6996edafe135ac7d263f74986de7972a238c
GouravShDev <74348508+GouravShDev@users.noreply.github.com>
2022-11-26 19:31:28 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (17.24%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -2,35 +2,26 @@ class Solution {
 public:
     vector<int> dp;
     int fact(int n){
-        if(n == 0)return 1;
+        if(n == 0) return 1;
         if(n == 1) return 1;
         if(n == 2) return 2;
-        if(dp[n] != -1) return dp[n];
-        
+        if(dp[n]!=-1) return dp[n];
         return dp[n] = n * fact(n-1);
     }
-    void solve(vector<int> numbers, int k , string &ans){
-        if(numbers.size() == 1){
-            ans += to_string(numbers.back());
-            return;
-        }
-        int n = numbers.size();
-        int factN = fact(n-1);
-        int x =  k / factN;
-        
-        ans += to_string(numbers[x]);
-        numbers.erase(numbers.begin() + x);
-        solve(numbers, k % factN, ans);
-        
+    void solve(int n , int k, vector<int> &sequence, string &ans){
+        if( n == 0 )return ;
+        int factN= fact(n-1);
+        int num = sequence[k / factN];
+        ans+= to_string(num);
+        sequence.erase( sequence.begin() + (k/factN));
+        solve(n-1, k % factN , sequence, ans);
     }
     string getPermutation(int n, int k) {
-        string ans = "";
-        vector<int> st;
-        dp.resize(n+1, -1);
-        for(int i =  1; i <= n; i++){
-            st.push_back(i);
-        }
-        solve(st, --k, ans);
+        dp.resize(n+1,-1);
+        vector<int> sequence(n);
+        string ans="";
+        iota(sequence.begin(), sequence.end() , 1);
+        solve(n , --k , sequence, ans);
         return ans;
     }
 };
