ce44d883f4a7317c75601caa830d9aa199a76808
GouravShDev <74348508+GouravShDev@users.noreply.github.com>
2022-11-28 22:38:38 +0530

    Time: 147 ms (73.29%), Space: 22.4 MB (89.10%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -6,9 +6,9 @@ public:
         sort(pairs.begin() , pairs.end(),[] (auto &a , auto &b){
             return a[1] < b[1];
         });
-        for(auto &p : pairs){
-            cout<<""<<p[0]<<","<<p[1]<<"} ";
-        }
+        // for(auto &p : pairs){
+        //     cout<<""<<p[0]<<","<<p[1]<<"} ";
+        // }
         for(int i =0 ;i < pairs.size() ; i++){
             auto index = prev(lower_bound(dp.begin(), dp.end(), make_pair(pairs[i][0], 0) ) ) - dp.begin();
             if(dp[index].second + 1 > dp.back().second){
