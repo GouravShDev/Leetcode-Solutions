67c74f1598d21fc6a3b8e9201adf220a0bd6a522
GouravShDev <74348508+GouravShDev@users.noreply.github.com>
2022-12-02 20:15:06 +0530

    Time: 316 ms (26.35%), Space: 20.6 MB (64.37%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -1,29 +1,17 @@
 class Solution {
 public:
     bool closeStrings(string word1, string word2) {
-        if(word1.size() != word2.size())return false;
-        map<char,int> mp1;
-        map<char,int> mp2;
+        vector<int> mp1(26,0), mp2(26,0), mp3(26,0), mp4(26,0);
         for(auto &ch : word1){
-            mp1[ch]++;
+            mp1[ch-'a']++;
+            mp3[ch-'a'] = 1;
         }
         for(auto &ch : word2){
-            if(mp1.find(ch) == mp1.end() )return false;
-            mp2[ch]++;
+            mp2[ch-'a']++;
+            mp4[ch-'a'] = 1;
         }
-        for(auto &ch : word1){
-            if(mp2.find(ch) == mp2.end()) return false;
-        }
-        
-        map<char, int> mp3;
-        map<char, int> mp4;
-        for(char c = 'a'; c <= 'z'; c++){
-            if(mp1[c] > 0) mp3[mp1[c]]++;
-            if(mp2[c] > 0) mp4[mp2[c]]++;
-        }
-        
-        return mp3 == mp4;
-        
-        
+        sort(mp1.begin(), mp1.end());
+        sort(mp2.begin(), mp2.end());
+        return mp1 == mp2 && mp3 == mp4;
     }
 };
