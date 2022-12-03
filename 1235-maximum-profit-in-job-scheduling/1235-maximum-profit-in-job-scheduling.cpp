fbc797a1c67211259d05bd28d23842086a5bfe17
GouravShDev <74348508+GouravShDev@users.noreply.github.com>
2022-12-04 01:57:15 +0530

    Time: 322 ms (47.34%), Space: 50.4 MB (70.88%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -1,52 +1,26 @@
-class Job{
-public:
-    int startTime;
-    int endTime;
-    int profit;
-    Job(int st, int et, int p){
-        startTime = st;
-        endTime= et;
-        profit= p;
-    }
-};
-
 class Solution {
 public:
-    // find job with max profit before startTime
-    int searchMxProfit(vector<pair<int,int>> &dp, int startTime){
-        int start = 0;
-        int end = dp.size()-1;
-        int ans =0;
-        while(start <= end){
-            int mid = start + (end - start)/2;
-            if(dp[mid].first<= startTime){
-                ans = mid;
-                start = mid + 1;
-            }else{
-                end = mid-1;
-            }
-        }
-        return ans;
-    }
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
-        int n = profit.size();
-        // pair < endTime, profit> 
-        vector<pair<int , int>>  dp;
-        vector<Job> jobs;
-        for(int i = 0; i < n ; i++){
-            jobs.push_back(Job(startTime[i], endTime[i], profit[i]));
+        vector<pair<int,int> > dp;
+        vector<pair<int, pair<int,int> >> vec;
+        for(int i =0 ;i < startTime.size(); i++){
+            vec.push_back({endTime[i], {startTime[i], profit[i]}});
         }
-        sort(jobs.begin() , jobs.end(),[](auto &a, auto &b){
-           return a.endTime < b.endTime; 
-        });
-        dp.push_back({0,0});
-        for(int i =0  ;i < n ; i++){
-            auto job= jobs[i];
-            int take = searchMxProfit(dp , job.startTime);
-            if(dp[take].second + job.profit > dp.back().second){
-                dp.push_back({job.endTime, dp[take].second + job.profit});
+        dp.push_back({0, 0});
+        sort(vec.begin() , vec.end());
+        for(int i = 0; i < vec.size(); i++){
+            auto end = vec[i].first;
+            auto [st, pft] = vec[i].second;
+            auto it = prev(upper_bound(dp.begin(), dp.end(), make_pair(st, INT_MAX)));
+            // for(auto & it : dp){
+            //     cout<<it.first<<" "<<it.second << endl;
+            // }
+            // cout<<st<<" => " << it->second<<endl;
+            if(dp.back().second < pft + it->second){
+                dp.push_back({end, pft + it->second});
             }
         }
         return dp.back().second;
     }
+    
 };
