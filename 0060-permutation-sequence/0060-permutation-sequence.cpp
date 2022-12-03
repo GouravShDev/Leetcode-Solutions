b51c5e757a4865fe60401e07c8742ceede48b23f
GouravShDev <74348508+GouravShDev@users.noreply.github.com>
2022-12-03 23:32:48 +0530

    Time: 7 ms (34.63%), Space: 6 MB (50.99%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -5,23 +5,22 @@ public:
         if(n == 0) return 1;
         if(n == 1) return 1;
         if(n == 2) return 2;
-        if(dp[n]!=-1) return dp[n];
+        if(dp[n] != -1) return dp[n];
         return dp[n] = n * fact(n-1);
     }
-    void solve(int n , int k, vector<int> &sequence, string &ans){
-        if( n == 0 )return ;
-        int factN= fact(n-1);
-        int num = sequence[k / factN];
-        ans+= to_string(num);
-        sequence.erase( sequence.begin() + (k/factN));
-        solve(n-1, k % factN , sequence, ans);
+    void solve(int n , int k , vector<int>& cur ,string &ans){
+        if(n == 0) return;
+        int factN = fact(n-1);
+        ans += to_string(cur[k / factN]);
+        cur.erase(cur.begin() + (k/factN));
+        solve(n-1,  k%factN , cur, ans);
     }
     string getPermutation(int n, int k) {
-        dp.resize(n+1,-1);
-        vector<int> sequence(n);
-        string ans="";
-        iota(sequence.begin(), sequence.end() , 1);
-        solve(n , --k , sequence, ans);
+        dp.resize(n+1, -1);
+        vector<int> nums(n);
+        iota(nums.begin(), nums.end() ,1);
+        string ans = "";
+        solve(n , k-1 , nums, ans);
         return ans;
     }
 };
