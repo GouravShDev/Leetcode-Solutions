3b84460350f0e3f0bd2e45355f4806f004ed435e
GouravShDev <74348508+GouravShDev@users.noreply.github.com>
2022-12-04 02:05:08 +0530

    Time: 364 ms (39.41%), Space: 60 MB (41.37%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
-        vector<pair<int,int> > dp;
-        vector<pair<int, pair<int,int> >> vec;
-        for(int i =0 ;i < startTime.size(); i++){
-            vec.push_back({endTime[i], {startTime[i], profit[i]}});
+        vector<pair<int,int>> dp;
+        vector<vector<int>> jobs;
+        for(int i = 0 ; i < startTime.size(); i++){
+            jobs.push_back({endTime[i], startTime[i], profit[i]});
         }
-        dp.push_back({0, 0});
-        sort(vec.begin() , vec.end());
-        for(int i = 0; i < vec.size(); i++){
-            auto end = vec[i].first;
-            auto [st, pft] = vec[i].second;
-            auto it = prev(upper_bound(dp.begin(), dp.end(), make_pair(st, INT_MAX)));
-            // for(auto & it : dp){
-            //     cout<<it.first<<" "<<it.second << endl;
-            // }
-            // cout<<st<<" => " << it->second<<endl;
-            if(dp.back().second < pft + it->second){
-                dp.push_back({end, pft + it->second});
+        sort(jobs.begin(), jobs.end());
+        dp.push_back({0,0});
+        for(auto &job : jobs){
+            int end = job[0];
+            int start = job[1];
+            int prf = job[2];
+            auto it = prev(upper_bound(dp.begin() , dp.end(), make_pair(start , INT_MAX)));
+            if(dp.back().second < it->second + prf){
+                dp.push_back({end, it->second + prf});
             }
         }
         return dp.back().second;
     }
-    
 };
