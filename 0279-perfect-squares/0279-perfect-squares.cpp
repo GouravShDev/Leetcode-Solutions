c07bec5b47b1a0e26e6b17cdbca059fb7260f1be
GouravShDev <74348508+GouravShDev@users.noreply.github.com>
2022-12-05 16:01:20 +0530

    Time: 99 ms (84.40%), Space: 9.1 MB (63.72%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
-    vector<int> sqrs;
     int numSquares(int n) {
-        int mxSq = 1001;
-        sqrs.resize(mxSq);
-        for(int i = 0;  i < mxSq; i++){
-            sqrs[i] = i * i;
-        }
-        queue<int> q;
-        vector<int> dp(mxSq * 10, INT_MAX);
-        q.push(n);
-        int level = 0;
-        while(!q.empty()){
-            int size = q.size();
-            for(int i =0 ;i  < size; i++){
-                int cur = q.front();
-                q.pop();
-                if(cur == 0) return level;
-                for(int j = 0;j  < 1001 && cur >= sqrs[j]; j++){
-                    if(dp[cur- sqrs[j]] > level){
-                        q.push(cur - sqrs[j]);
-                        dp[cur-sqrs[j]] = level;
-                    }
-                }
+        vector<int> dp(n+1);
+        dp[1]= 1;
+        for(int i = 1 ; i <= n ; i++){
+            int minNum = INT_MAX;
+            int c = 1;
+            for(int j = 1; j <= i ; j = c * c){
+                minNum = min(minNum, 1 + dp[i-j]);
+                c++;
             }
-            level++;
+            dp[i] = minNum;
         }
-        return -1;
+        
+        return dp[n];
     }
 };
