97c2d2a88db6b2bb592fbac2a41a49136a114987
GouravShDev <74348508+GouravShDev@users.noreply.github.com>
2022-12-29 22:47:41 +0530

    Time: 536 ms (97.88%), Space: 126.6 MB (81.75%) - LeetHub

---
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
@@ -0,0 +1,59 @@
+class Task{
+    public:
+    int id;
+    int eqTime;
+    int pTime;
+    Task(int t_id, int e, int p){
+        id = t_id;
+        eqTime = e;
+        pTime = p;
+    }
+};
+struct compare{
+    bool operator()(const Task &a, const Task &b){
+        if(a.pTime == b.pTime){
+            return a.id > b.id;
+        }
+        return a.pTime > b.pTime;
+    }
+};
+class Solution {
+public:
+    vector<int> getOrder(vector<vector<int>>& tasks) {
+        priority_queue<Task, vector<Task > , compare > pq;
+        int n = tasks.size();
+        vector<Task> tsks;
+        for(int i =0 ;i < n; i++){
+            tsks.push_back(Task(i , tasks[i][0], tasks[i][1]));
+        }
+        sort(tsks.begin() , tsks.end(), [](auto a, auto b){
+            return a.eqTime < b.eqTime;
+        });
+        // for(auto & ts : tsks){
+            // cout<<ts.eqTime<<" ";
+        // }
+        int i = 0;
+        long long curTime = 0;
+        vector<int> ans(n);
+        int k = 0;
+        while(!pq.empty() || i < n){ 
+            
+            while(i < n && curTime >= tsks[i].eqTime){
+                pq.push(tsks[i++]);
+            }
+            if(pq.empty()){
+                curTime = tsks[i].eqTime;
+                pq.push(tsks[i++]);
+                while(i < n && curTime >= tsks[i].eqTime){
+                    pq.push(tsks[i++]);
+                }
+            }
+           
+            ans[k++] = pq.top().id;
+            curTime += 1ll * pq.top().pTime;
+            // cout<<curTime<<" ";
+            pq.pop();
+        }
+        return ans;
+    }
+};
