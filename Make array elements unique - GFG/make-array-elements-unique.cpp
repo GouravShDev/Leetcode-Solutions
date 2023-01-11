702ee3166f2f8091fd63cb497238381fd1c56a67
GouravShDev <74348508+GouravShDev@users.noreply.github.com>
2023-01-11 22:57:59 +0530

    Added solution - LeetHub

--- Make array elements unique - GFG/make-array-elements-unique.cpp
+++ Make array elements unique - GFG/make-array-elements-unique.cpp
@@ -12,13 +12,16 @@ class Solution {
     long long int minIncrements(vector<int> arr, int N) {
         sort(arr.begin() , arr.end());
         long long ans = 0;
+        int lastAdded = 0;
         for(int i = 1  ;i < arr.size() ; i++){
-            if(arr[i-1] == arr[i]){
-                arr[i]++;
+            if(arr[i-1]+lastAdded == arr[i]){
+                lastAdded = 1;
                 ans++;
-            }else if(arr[i-1] > arr[i]){
-                ans +=  arr[i-1] - arr[i] +1;
-                arr[i] = arr[i-1]+1;
+            }else if(arr[i-1]+lastAdded > arr[i]){
+                lastAdded =  arr[i-1] + lastAdded - arr[i] +1;
+                ans += lastAdded;
+            }else{
+                lastAdded = 0;
             }
         }
         return ans;
