45979776fbc17e489b96b3e0a4a617729fc5ebf8
GouravShDev <74348508+GouravShDev@users.noreply.github.com>
2023-01-26 11:51:51 +0530

    Time: 44 ms (42.45%), Space: 13.1 MB (75.69%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,35 +1,37 @@
-class Solution
-{
-    public:
-        int findCheapestPrice(int n, vector<vector < int>> &flights, int src, int dst, int k) {
-            vector<pair<int,int>> graph[n];
-            for(int i =0 ; i < flights.size(); i++){
-                int u = flights[i][0];
-                int v = flights[i][1];
-                int weight = flights[i][2];
-                graph[u].push_back({v, weight});
-            }
-            queue<pair<int, pair<int,int>>> pq;
-            
-            pq.push({0, {src, k}});
-            vector<int> costs(n, 1e8);
-            costs[src] = 0;
-            while(!pq.empty()){
-                int cost = pq.front().first;
-                int node = pq.front().second.first;
-                int steps = pq.front().second.second;
-                pq.pop();
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<pair<int,int>> graph[n];
+        for(int i =0  ; i < flights.size() ; i++){
+            int u  = flights[i][0];
+            int v = flights[i][1];
+            int price = flights[i][2];
+            graph[u].push_back({v, price});
+        }
+        queue<pair<int,int>> q;
+        vector<int> prices(n, INT_MAX);
+        prices[src] = 0;
+        int level = 0;
+        q.push({ 0, src});
+        while(!q.empty()){
+            int sz = q.size();
+            for(int i = 0;i < sz; i++){
+                int node = q.front().second;
+                int price = q.front().first;
+                q.pop();
+                if( k < level) continue;
                 for(auto &it : graph[node]){
-                    int adjNode = it.first;
-                    int adjCost = it.second;
-                    
-                    if(steps >= 0 && adjCost + cost < costs[adjNode]){
-                        costs[adjNode] = adjCost + cost;
-                        pq.push({costs[adjNode], {adjNode, steps-1}});
+                    int nd = it.first;
+                    int p = it.second;
+                    if(p + price < prices[nd]){
+                     prices[nd] = p + price;
+                        q.push({p + price, nd});   
                     }
+                    
                 }
             }
-            if(costs[dst] != 1e8) return costs[dst];
-            return -1;
+            level++;
         }
+        return prices[dst] == INT_MAX ? -1 : prices[dst];
+    }
 };
