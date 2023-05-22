888fe433dd2b88ba7d2e3ee92055ab31ff840252
GouravShDev <74348508+GouravShDev@users.noreply.github.com>
2023-05-22 22:58:14 +0530

    Time: 15 ms (82.47%), Space: 17.1 MB (7.12%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         vector<int> freqMp((1e4 * 2)+ 2);
-        vector< vector<int>> valuesMp(1e4 * 2+ 2);
+        vector< vector<int>> valuesMp(nums.size() + 2);
         for(auto &num : nums) freqMp[num + 1e4]++;
         for(int i =0 ;i < 1e4 * 2 + 2 ; i++){
             if(freqMp[i])valuesMp[freqMp[i]].push_back(i - 1e4);
