34ed4a0737ae9b647d7858c6e3ea763bcb25126b
GouravShDev <74348508+GouravShDev@users.noreply.github.com>
2023-05-22 22:54:40 +0530

    Time: 153 ms (5.30%), Space: 51.2 MB (7.12%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        vector<int> freqMp((1e4 * 2)+ 2);
+        unordered_map<int,int> freqMp;
         vector< vector<int>> valuesMp(1e4 * 2+ 2);
         for(auto &num : nums) freqMp[num + 1e4]++;
         for(int i =0 ;i < 1e4 * 2 + 2 ; i++){
