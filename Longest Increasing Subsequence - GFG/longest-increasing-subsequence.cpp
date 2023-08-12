a92dfd5053ff56063180f2f0a41c469e230200da
GouravShDev <74348508+GouravShDev@users.noreply.github.com>
2023-08-12 13:47:03 +0530

    Added solution - LeetHub

---
+++ Longest Increasing Subsequence - GFG/longest-increasing-subsequence.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to find length of longest increasing subsequence.
+    int longestSubsequence(int n, int a[])
+    {
+        vector<int> ans;
+        for(int i= 0 ; i < n ; i++){
+            if(!ans.empty() && a[i] <= ans.back()){
+                auto itr= lower_bound(ans.begin(), ans.end(), a[i]);
+                *itr = a[i];
+                continue;
+            }
+            ans.push_back(a[i]);
+        }
+        return ans.size();
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    //taking total testcases
+    int t,n;
+    cin>>t;
+    while(t--)
+    {
+        //taking size of array
+        cin>>n;
+        int a[n];
+        
+        //inserting elements to the array
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        Solution ob;
+        //calling method longestSubsequence()
+        cout << ob.longestSubsequence(n, a) << endl;
+    }
+}
+
+// } Driver Code Ends
