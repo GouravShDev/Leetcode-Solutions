47d71c25a691a8c87c15f9a3e3d941da571db026
GouravShDev <74348508+GouravShDev@users.noreply.github.com>
2023-10-09 21:51:33 +0530

    Time: 11 ms (9.25%), Space: 14.2 MB (8.57%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,11 +1,25 @@
 class Solution {
 public:
+    int bs(vector<int> &nums, int target, bool lastOcc){
+        int start = 0;
+        int end = nums.size()-1;
+        int ans = -1;
+        while(start <= end){
+            int mid = (start + end)/2;
+            if(nums[mid] == target){
+                ans = mid;
+                if(lastOcc) start = mid +1;
+                else end = mid -1;
+            }else
+            if(nums[mid] < target){
+                start = mid+1;
+            }else{
+                end = mid -1;
+            }
+        }
+        return ans;
+    }
     vector<int> searchRange(vector<int>& nums, int target) {
-        if(nums.empty()) return {-1, -1};
-        int leftIndex = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
-        int rightIndex = upper_bound(nums.begin(), nums.end(), target) - nums.begin();
-        if(leftIndex >= nums.size() || nums[leftIndex] != target) return {-1, -1};
-        return {leftIndex, rightIndex-1};
-        
+        return {bs(nums, target, false), bs(nums,target, true)};
     }
 };
