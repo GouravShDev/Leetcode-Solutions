649bfb5425b1e97f89a7e019984307acb0a22a98
GouravShDev <74348508+GouravShDev@users.noreply.github.com>
2023-10-26 23:54:53 +0530

    Time: 142 ms (9.45%), Space: 16.9 MB (16.54%) - LeetHub

--- 0823-binary-trees-with-factors/0823-binary-trees-with-factors.cpp
+++ 0823-binary-trees-with-factors/0823-binary-trees-with-factors.cpp
@@ -4,19 +4,19 @@ public:
         sort(arr.begin(), arr.end());
         int ans = 0;
         map<int,int> mp;
-        int mod= 1e9+7;
+        int mod = 1e9 + 7;
         for(int i = 0 ;i < arr.size(); i++){
             mp[arr[i]] = 1;
             for(int j = 0; j < i; j++){
                 //if(arr[j] * arr[j] > arr[i]) break;
                 int div = arr[i] / arr[j];
-                if((arr[i] % arr[j]) == 0){
-                    //int cur =  mp[arr[j]];
-                    //if(arr[j] != div) cur = ( (long long)cur * mp[div]) % mod;
-                    mp[arr[i]] = ( mp[arr[i]] + ( (long long) mp[arr[j]] * mp[div] ) % mod ) % mod;
+                if(arr[i] % arr[j] == 0 && arr[i] % div == 0){
+                    int cur =  mp[arr[j]];
+                     cur = ( (long long) cur * mp[div]) % mod;
+                    mp[arr[i]] = (mp[arr[i]]  + cur ) % mod;
                 }
             }
-            ans = (ans + mp[arr[i]]) % mod;
+            ans =  (ans + mp[arr[i]]) % mod;
         }
         return ans;
     }
